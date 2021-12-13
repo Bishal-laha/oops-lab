12dd6d38945bbcf2205bace97fbccf23263cf7b6
Bishal-laha <bishallaha12345@gmail.com>
2021-12-14 02:01:21 +0530

    Delete 3 directory

--- 3/3.3 Other Version/A.java
+++
@@ -1,6 +0,0 @@
-interface A{
-
-	public void add(int x,int y);
-	public void sub(int x,int y);
-
-}
--- 3/3.3 Other Version/S.java
+++
@@ -1,6 +0,0 @@
-interface S{
-
-	public void add(int x,int y);
-	public void sub(int x,int y);
-	public void expo(int x,int y);
-}
